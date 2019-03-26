265b7632da11b90c5a73033a8280bcab25d3d584
durgeshbhagat <durgeshit@gmail.com>
2019-03-26 16:32:26 +0530

    Adding IG setup for DatASET-3

--- Special_words_LDA/calling_lda_d_ver-7.py
+++ Special_words_LDA/calling_lda_d_ver-7.py
@@ -12,40 +12,41 @@ t1=time.time()
 
 l_per = []
 
-eta_2 = 0.3 # float(sys.argv[1]) # Non special word ; eta_! : special word
+eta_2 = 0. # float(sys.argv[1]) # Non special word ; eta_! : special word
 
 eta_1_start = 1 #int(sys.argv[2])
-eta_1_end = 6 #int(sys.argv[3])
+eta_1_end = 2 #int(sys.argv[3])
 
-setup_list = ['ner_keywords', 'tf-df-icf']
+eta_2_start = 2 #int(sys.argv[2])
+eta_2_end = 3 #int(sys.argv[3])
+
+setup_list = ['ner_keywords', 'tf-df-icf', 'IG']
 
 k =40
 
+no_of_topic = 20
+did = 'Dataset-3'
 
-
-for temp_eta_2 in range(1,6):
-    eta_2 = temp_eta_2 * 0.1
-    setup = 'tf-df-ief-%d_eta_2-%0.2f' %(k,eta_2) # 'ner_keywords' # changed Here ! ----k =40
-    no_of_topic = 20
-    setup =  'tf-df-ief-%d_eta_2-%0.2f' %(k,eta_2) #'ner_keywords' #'tf-df-ief-%d_eta_2-%0.2f' %(k,eta_2) #Changed here ! ...
-    did = 'Dataset-3'
-
-    for eta_1 in range(eta_1_start, eta_1_end):
-        temp_eta1 = eta_1* 0.1
-        for index, cur_setup in enumerate(setup_list):
+for cur_eta_1 in range(eta_1_start, eta_1_end):
+    for cur_eta_2 in range(eta_2_start, eta_2_end):
+        eta_1 = cur_eta_1* 0.1
+        eta_2 = cur_eta_2 * 0.1
+        for index, cur_setup in enumerate(setup_list[2:3],2):
             if index == 0:
-                fsp_file = '%s/ner_keywords/%s-ner_keyword_%0.1f' % (did, did, temp_eta1)
+                fsp_file = '%s/ner_keywords/%s-ner_keyword_%0.1f' % (did, did, eta_1)
                 setup = 'ner_keywords'
             elif index == 1:
-                fsp_file = '%s/tf-df-icf/top-%d/%s-tf-df-icf_weight_%0.2f' % (did, k, did, temp_eta1)
+                fsp_file = '%s/tf-df-icf/top-%d/%s-tf-df-icf_weight_%0.2f' % (did, k, did, eta_1)
                 setup = 'tf-df-icf_top-%d' % (k)
+            elif index ==2:
+                fsp_file = '%s/IG/top-%d/%s-IG_weight_%0.2f' % (did, k, did, eta_1)
+                setup = 'IG_top-%d' %(k)
 
-            eta_1 = temp_eta1 * 0.1
             finp_file = '20-Newsgroup/20-Newsgroup_train_stemmed.pkl'
 
             iter_count = 120
             cmd = 'python lda_d_ver-1.py   --alpha=0.1 --eta1=%0.2f --eta2=%0.2f --fin=%s --fsp=%s -s -i %d --dp=%s --setup=%s --datasets=%s -k %d' \
-                  % (temp_eta1, eta_2, finp_file, fsp_file, iter_count, 'uniform', setup, did, no_of_topic)
+                  % (eta_1, eta_2, finp_file, fsp_file, iter_count, 'uniform', setup, did, no_of_topic)
             print(' ##:', cmd)
             os.system(cmd)
 
